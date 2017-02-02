4c92ffebf8c6c0ca4ab87d4d475728661937af2b
payamamirani <the.boy.who.live.70@gmail.com>
2017-02-02 22:35:39 +0330

    Complete Categories

--- public/app/admin/category/mvCategoryCtrl.js
+++ public/app/admin/category/mvCategoryCtrl.js
@@ -1,3 +1,63 @@
-angular.module('app').controller('mvCategoryCtrl', function($scope, mvCategory) {
+angular.module('app').controller('mvCategoryCtrl', function($scope, mvCategory, mvNotifier) {
     $scope.categories = mvCategory.query();
+    $scope.SaveCategory = function () {
+        var categoryData = {
+            Title: $scope.categoryName
+        };
+
+        switch ($scope.method) {
+            case "AddChild":
+                categoryData.ParentId = $scope.parentId;
+                break;
+            case "EditNode":
+                categoryData.Id = $scope.Id;
+                categoryData.ParentId = $scope.parentId;
+                break;
+        }
+
+        var newCategory = new mvCategory(categoryData);
+        if ($scope.method !== "EditNode") {
+            newCategory.$save().then(function () {
+                mvNotifier.successNotify(texts.SuccessAction, "");
+                $scope.categories = mvCategory.query();
+            }, function (response) {
+                mvNotifier.errorNotify(texts.ErrorAction, response.data.reason);
+            })
+        } else {
+            newCategory.$update().then(function () {
+                mvNotifier.successNotify(texts.SuccessAction, "");
+                $scope.categories = mvCategory.query();
+            }, function (response) {
+                mvNotifier.errorNotify(texts.ErrorAction, response.data.reason);
+            })
+        }
+    };
+
+    $scope.AddNew = function () {
+        $("#AddCategory").modal('show');
+        $scope.parentId = null;
+        $scope.Id = null;
+        $scope.categoryName = null;
+        $scope.method = "AddNew";
+        $scope.ModalTitle = texts.AddCategory;
+    };
+    $scope.AddChild = function (category) {
+        $("#AddCategory").modal('show');
+        $scope.parentId = category._id;
+        $scope.Id = null;
+        $scope.categoryName = null;
+        $scope.method = "AddChild";
+        $scope.ModalTitle = texts.AddChildCategory;
+    };
+    $scope.EditNode = function (category) {
+        $("#AddCategory").modal('show');
+        $scope.parentId = "Edit Child";
+        $scope.Id = category._id;
+        $scope.categoryName = category.Title;
+        $scope.method = "EditNode";
+        $scope.ModalTitle = texts.EditCategory;
+    };
+    $scope.DeleteChild = function (category) {
+
+    };
 });
--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -10,17 +10,53 @@ exports.getAllCategories = function (req, res) {
 exports.createCategories = function(req, res) {
     var categoryData = req.body;
     categoryData.CreatedBy = req.user[0].Username.toLowerCase();
-
-    CategoriesModel.create(categoryData, function (err, category) {
-        if(err) {
-            res.status(400);
-            return res.send({reason: err.toString()});
-        } else {
-            res.send(category);
-        }
-    });
+    if(!categoryData.ParentId) {
+        CategoriesModel.create(categoryData, function (err, category) {
+            if (err) {
+                res.status(400);
+                return res.send({reason: err.toString()});
+            } else {
+                res.send(category);
+            }
+        });
+    } else {
+        var child = new CategoriesModel();
+        child.Title = categoryData.Title;
+        child.CreatedBy = req.user[0].Username.toLowerCase();
+        CategoriesModel.findOneAndUpdate({_id: categoryData.ParentId}, { $push: { Child: child }} ,
+            {safe:true, upsert: true}, function(err, category) {
+            if(err) {
+                res.status(400);
+                return res.send({reason: err.toString()});
+            } else {
+                res.send(category);
+            }
+        });
+    }
 };
 
 exports.updateCategories = function (req, res) {
-    res.send({success: true});
+    var categoryData = req.body;
+    if(!categoryData.ParentId) {
+        CategoriesModel.findOneAndUpdate({_id: categoryData.Id}, {$set: {Title: categoryData.Title}}, {new: true},
+            function (err, category) {
+                if (err) {
+                    res.status(400);
+                    return res.send({reason: err.toString()});
+                } else {
+                    res.send(category);
+                }
+            });
+    } else {
+        CategoriesModel.update({"Child._id": categoryData.Id},
+            {$set: {'Child.$.Title': categoryData.Title}}, {new: true},
+            function (err, category) {
+                if (err) {
+                    res.status(400);
+                    return res.send({reason: err.toString()});
+                } else {
+                    res.send(category);
+                }
+            });
+    }
 };

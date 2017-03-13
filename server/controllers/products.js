77366351fe70c33816a44a7de54e26b171e3f962
payamamirani <the.boy.who.live.70@gmail.com>
2017-03-14 00:38:58 +0330

    Test Telegram

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,8 +1,28 @@
 
-var ProductsModel = require('mongoose').model('Product');
+var ProductsModel = require('mongoose').model('Product'),
+    axios = require('axios'),
+    token = "348250619:AAF5YuCpO1KQACUunCn6TL0x1ebP0e0yuww";
+
+function TelegramBot(token, method, data, res) {
+    axios.post('https://api.telegram.org/bot' + token + '/' + method , data)
+        .then(function (response) {
+            console.log("OK" + response);
+            res.end();
+        })
+        .catch(function(err) {
+            console.error(err);
+            res.end(err);
+        });
+}
 
 exports.test = function (req, res) {
-    return res.send({ok:true, result: 'hello'});
+    var data = req.body;
+    if(data.ok) {
+        TelegramBot(token, 'sendMessage',
+            {chat_id: data.chat.id, text: 'This is a test', reply_to_message_id: data.message.id}, res)
+    } else {
+        return res.send({ok: false});
+    }
 };
 
 exports.getAllProducts = function (req, res) {
